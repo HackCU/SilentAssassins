1da9403428551871ab4f2229437ec308760f07d2
Siva Palakurthi <siva.palakurthi@colorado.edu>
2015-04-11 04:09:37 -0600

    analysis done without summarizer

--- Analysis.py
+++ Analysis.py
@@ -1,15 +1,49 @@
+# -*- coding: utf-8 -*-
+#input = {'privacy':{'p':[{'name':'Our privacy policy explains how we treat your personal data and protect your privacy when you use our Services. By using our Services, you agree that Google can use such data in accordance with our privacy policy.'}
+#                        ,{'name':"You will ensure that at all times you use the Services, the Properties have a clearly labeled and easily accessible privacy policy that provides end users with clear and comprehensive information about cookies, device-specific information, location information and other information stored on, accessed on, or collected from end users’ devices in connection with the Services, including, as applicable, information about end users’ options for cookie management.  You will use commercially reasonable efforts to ensure that an end user gives consent to the storing and accessing of cookies, device-specific information, location information or other information on the end user's device in connection with the Services where such consent is required by law."}] },
+#        'taxes':{'p':[{'name':'As between you and Google, Google is responsible for all taxes (if any) associated with the transactions between Google and advertisers in connection with Ads displayed on the Properties.  You are responsible for all taxes (if any) associated with the Services, other than taxes based on Google’s net income.  All payments to you from Google in relation to the Services will be treated as inclusive of tax (if applicable) and will not be adjusted.'}] }
+#       }      
+
 
 from aylienapiclient import textapi
+import json
 
 class Analysis:
     def __init__(self):
        self.client = textapi.Client("a3d83921", "e6f553b78d258d7d17b7037bf5e94425")
  
-    def analysis(self, text):
+    def analysis(self, obj):
         
-        classify = self.client.Classify({"text": text})
-        concepts = self.client.Concepts({"text": text})
-        entities = self.client.Entities({"text": text})
-        hashtags = self.client.Hashtags({"text": text})
-        
-    
+        final_hash = {}
+        names = []
+        for n in input.keys():
+		    names.append(n)
+        for i in names:
+    		para_text=""
+    		paras_list = []
+    		para = input[i]
+    		list_para = para['p']
+    		length = len(list_para)
+    		for j in range(0,length):
+    			p = list_para[j]
+    			hashtags = self.client.Hashtags({"text": p['name']})
+    			par_hash = {}
+    			par_hash["name"] = p['name']
+	        	par_hash["tags"] = hashtags['hashtags']
+	        	paras_list.append(par_hash)
+
+	        for ind_para in paras_list:
+	        	para_text+=ind_para['name']
+
+	        classify = self.client.Classify({"text": para_text})
+
+	        final_hash[i]={}
+	        final_hash[i]["classify"]=classify['categories'][0]['label']
+	        final_hash[i]['p'] = paras_list
+
+	#print final_hash
+        return final_hash
+        #summarize = self.client.Summarize({"text": para_text,"title":"policy"})
+
+#a = Analysis()
+#obj = Analysis.analysis(a,input)
--- Generator.py
+++ Generator.py
@@ -1,8 +1,18 @@
+#obj = json.dumps({[{'head':{'name':'privacy','classify':'computer software','p':{'name':'first para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},'p':{'name':'second para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']} }},
+#    {'head':{'name':'taxes','classify':'computer software','p':{'name':'first para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},'p':{'name':'second para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']} }}
+#]})
+
+
+##### Input Format
+#obj = json.dumps({'privacy':{'classify':'computer software','p':[{'name':'first para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
+#         'taxes':{'classify':'computer software','p':[{'name':'first para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
+#         'something':{'classify':'cat 2','p':[{'name':'first para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
+#         'some other thing':{'classify':'cat 3','p':[{'name':'first para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] }
+#        })
+
 import json
 
 class Generator:
-    def __init__(self):
-       print ""
        
     def generate(self, obj):
         categories = {}
@@ -10,50 +20,27 @@ class Generator:
         for key,value in obj.iteritems():
             if(value['classify'] not in categories):
                 categories[value['classify']] = []
-            categories[value['classify']].append({'key':key,'value':value})
-            
+            categories[value['classify']].append({'key':key,'value':value})     
         final = {} 
         final['children'] = []  
         for level in categories:
-            #print level
             final['children'].append({ 'name': level,"children":self.getChilds(categories[level])})
         return final
                         
     def getChilds(self, level):
         children = [] 
-        print "--------------------------------------"
         for eachChild in level:
             child = {}
-            #print eachChild
-            #print "$$$$$$$$$$$$$$$$$$$$"
             child["name"] = eachChild["key"]
             child["children"] = []
-            #print eachChild['value']
             for eachP in eachChild['value']['p']:
-                #for p in eachP['p']:
                 node = {}
-                node["name"] = eachP['name']                
+                node["name"] = eachP['name']
+                node["tags"] = eachP['tags']                
+                node["summarizer"] = eachP['summarizer']
                 child["children"].append(node)    
-                #print eachP
             children.append(child)
         return children
-    
-#obj = json.dumps({[{'head':{'name':'privacy','classify':'computer software','p':{'name':'first para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},'p':{'name':'second para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']} }},
-#    {'head':{'name':'taxes','classify':'computer software','p':{'name':'first para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},'p':{'name':'second para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']} }}
-#]})
 
-obj = json.dumps({'privacy':{'classify':'computer software','p':[{'name':'first para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
-         'taxes':{'classify':'computer software','p':[{'name':'first para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
-         'something':{'classify':'cat 2','p':[{'name':'first para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
-         'some other thing':{'classify':'cat 3','p':[{'name':'first para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] }
-        })
-
-#obj = json.dumps({'privacy':{'classify':'computer software','p':[{'name':'first para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
-#         'taxes':{'classify':'computer software','p':[{'name':'first para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
-#         'something':{'classify':'cat 2','p':[{'name':'first para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in privacy','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] },
-#         'some other thing':{'classify':'cat 3','p':[{'name':'first para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']},{'name':'second para in taxes','summarizer':'example of summarized paragraph','tags':['tag1','tag2','tag3']}] }
-#        })
-
-#print obj 
-gen = Generator()
-print gen.generate(obj)
+#gen = Generator()
+#print gen.generate(obj)
